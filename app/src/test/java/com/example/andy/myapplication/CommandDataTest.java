66cb9e639b841f8b33744316be6a912743e6990c
girvain <gavinross88@hotmail.co.uk>
2019-10-10 22:34:45 +0100

    pushing git merge with branch

---
+++ app/src/test/java/com/example/andy/myapplication/CommandDataTest.java
@@ -0,0 +1,42 @@
+package com.example.andy.myapplication;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CommandDataTest {
+    CommandData commandData;
+
+    @Before
+    public void onSetup() {
+        commandData = new CommandData();
+    }
+
+    @Test
+    public void validateMissingCommand() {
+        commandData.getPositions().add(3);
+        commandData.getPositions().add(4);
+        commandData.setName(DrumTrackData.KICK);
+
+        assertFalse(commandData.validate());
+    }
+
+    @Test
+    public void validateNoMissingValues() {
+        commandData.setCommand(DrumTrackData.INSERT);
+        commandData.setName(DrumTrackData.KICK);
+        commandData.getPositions().add(3);
+
+        assertTrue(commandData.validate());
+    }
+
+    @Test
+    public void validatePositionsBoundryValues() {
+        commandData.setCommand(DrumTrackData.INSERT);
+        commandData.setName(DrumTrackData.KICK);
+        commandData.getPositions().add(-1);
+
+        assertFalse(commandData.validate());
+    }
+}
